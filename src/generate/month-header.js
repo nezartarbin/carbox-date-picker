bf02f70e170682f9185aa672251807a0c2551f91
Nezar Tarbin <nizar.t96@gmail.com>
2019-10-10 19:33:30 -0400

    added year to month header

--- src/generate/month-header.js
+++ src/generate/month-header.js
@@ -1,24 +1,24 @@
 import setup_day_grid from './day-grid';
 import { months } from '../names';
 
-function create_change_month_button(selected_time, text, offset, month_name, day_grid, input) {
+function create_change_month_button(selected_time, text, offset, month_year_text, day_grid, input) {
     const button = document.createElement("button");
     button.addEventListener("click", () => {
         selected_time.set_time(new Date(selected_time.get_time().getFullYear(), selected_time.get_time().getMonth()+offset));
         setup_day_grid(day_grid, selected_time, input);
-        month_name.textContent = months[selected_time.get_time().getMonth()];
+        month_year_text.textContent = months[selected_time.get_time().getMonth()];
     })
     button.textContent = text;
     return button;
 }
 
 export default function setup_month_header(month_header, day_grid, selected_time, input) {
-    const month_name = document.createElement("div");
-    month_name.className = "width-100px display-inline-block";
-    month_name.textContent = months[selected_time.get_time().getMonth()];
-    const backwardButton = create_change_month_button(selected_time, "<", -1, month_name, day_grid, input);
-    const forwardButton = create_change_month_button(selected_time, ">", +1, month_name, day_grid, input);
+    const month_year_text = document.createElement("div");
+    month_year_text.className = "width-100px display-inline-block";
+    month_year_text.textContent = `${months[selected_time.get_time().getMonth()]} ${selected_time.get_time().getFullYear()}`;
+    const backwardButton = create_change_month_button(selected_time, "<", -1, month_year_text, day_grid, input);
+    const forwardButton = create_change_month_button(selected_time, ">", +1, month_year_text, day_grid, input);
     
-    month_header.append(backwardButton, month_name, forwardButton);
+    month_header.append(backwardButton, month_year_text, forwardButton);
     return selected_time;
 }
