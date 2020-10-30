a1c5aeef9919a999c5ee2cb065b7b6503c36ba9a
nezartarbin <41840738+nezartarbin@users.noreply.github.com>
2020-10-29 22:27:24 -0400

    Implement Interface as a Web Component (#4)
    * change interface to web component
    
    * edit readme
    
    * update interface docs in readme

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import generate from './generate';
 
 const inputs = document.getElementsByClassName("carbox-picker");
 
-for (let i = 0, len = inputs.length; i < len; i++) {
-    let container = generate(inputs[i]);
-    inputs[i].addEventListener("click", e => e.stopPropagation());
-    inputs[i].addEventListener("focus", () => {
-        container.style.display = "block";
-    })
-    
-    window.addEventListener("click", e => {
-        container.style.display = "none";
-    })
+class Carbox_picker extends HTMLElement {
+    connectedCallback() {
+        const input = document.createElement("input");
+        input.type = "text";
+        this.appendChild(input);
+        const container = generate(input)
+        input.addEventListener("click", e => e.stopPropagation());
+        input.addEventListener("focus", () => {
+            container.style.display = "block";
+        })
+        window.addEventListener("click", e => {
+            container.style.display = "none";
+        })
+    }
 }
+
+customElements.define("carbox-picker", Carbox_picker)
